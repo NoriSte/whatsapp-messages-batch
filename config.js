4af376798f8efb1a70b5e1ec9315f0091df99803
Stefano Magni <nori.ste.magni@gmail.com>
2018-12-21 08:42:21 +0100

    added some comments

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // set it to true to let the script check if it "can" send messages without really sending them
+  TEST_SAFELY: false,
+
+  // the delay after every message sending
+  CHECK_DELAY: 1500,
+
+  // the title of the "Search or start a new chat" input field (see the input.jpg file)
+  findInputTitle: "Cerca o inizia una nuova chat"
+};
--- messages.js
+++ messages.js
@@ -1,7 +1,12 @@
 module.exports = {
   messages: [
-    ["Clark Kent", `Happy Xmas!! 🎄`],
+    // ['contact name', 'message']
     ["James Bond", `No one is going to know your phone number, don't worry!`],
+
+    // emojis are supported
+    ["Clark Kent", `Happy Xmas!! 🎄`],
+
+    // you can write multiline messages (every line will result in a dedicated message) leveraging es6 string literals
     [
       "Soccer group",
       `Wishes to you all!
